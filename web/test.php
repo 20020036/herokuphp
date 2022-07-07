581c8c33f8e29e62d8ae1a7e78f951115746acdd
abrahamelizabeth <20020036@myrp.edu.sg>
2022-07-07 12:41:02 +0800

    fixed SEO issues

--- web/test.php
+++ web/test.php
@@ -2,17 +2,25 @@
 
 Fixes:
 Category: Accessibility
-1. Document doesn't have a <title> element.
-2. <html> elements does not have a [lang] attribute.
+1. Document doesn't have a <title> element. (Fixed)
+2. <html> elements does not have a [lang] attribute. (Fixed)
 
+Category: Best Practices
+1. Does not use HTTPS (Not Fixed)
+2. Page lacks the HTML doctype, thus triggering quirks-mode. (Fixed)
+
+Category: SEO
+1. Does not have a <meta name="viewport"> tag with width or initial-scale. (Fixed)
+2. Document doesn't use legible font sizes. (Fixed)
 
 -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Test Page</title>
 </head>
-<body>
+<body style="font-size: 15px;">
   Hello World!
 </body>
 </html>
